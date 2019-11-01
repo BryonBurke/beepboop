dd6dca4dd36d5bf5218767246059f1b5b0ba3daa
Bryon Burke <bryonparis@gmail.com>
2019-11-01 11:58:20 -0700

    spec 2 business logic complete.

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,27 @@ $(document).ready(function() {
 
     // Get user input
         var userNumInt = parseInt($("#userNumEntry").val());
-        var userNumString = $("#userNumEntry").val();
-        var userNumStringSplit = userNumString.split('');
 
-    console.log(userNumStringSplit);
+
+
     // apply rules and return a list of numbers from 0 to the users inputted number
-        var outNum = ["0"];
+        var outNum = 11;
+
 
         for (var i =0; i < userNumInt; i++){
 
+          iArray = [i].toString('');
+          var iArraySplit = iArray.split('');
 
-    
+          if (iArraySplit.includes("1")){
+            console.log("beep");
+          }
+          else {
+            console.log(iArray);
+          }
 
-          console.log(outNum);
 
-          outNum ++
+
         }
 
 
