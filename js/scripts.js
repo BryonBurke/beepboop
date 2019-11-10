a5758bf7a0ed2511d10281b7dcbb8202d69c8c99
BryonBurke <bryonparis@gmail.com>
2019-11-09 16:34:08 -0800

    separate business and UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -3,55 +3,44 @@ $(document).ready(function() {
     event.preventDefault();
      $(".replyToInput").show();
 
+     // Business Logic
 
+     function makeResult (userNumInt){
 
-    // Get user input
-        var userNumInt = parseInt($("#userNumEntry").val());
-        var resultArray = []
+       var resultArray = []
 
-    // apply rules and return a list of numbers from 0 to the users inputted number. use the index for a display counter and compare input for rules
+       for (var i =0; i <= userNumInt; i++){
+         iArray = [i].toString('');
+         var iArraySplit = iArray.split('');
+         if  (  iArray === "13"   ) {
+           resultArray.push(" I'm sorry, Dave. I'm afraid I can't do that.");
+         }
+         else if  (  iArray === "21"   ) {
+           resultArray.push(" I'm sorry, Dave. I'm afraid I can't do that.");
+         }
+         else if (iArraySplit.includes("3")){
+           resultArray.push(" I'm sorry, Dave. I'm afraid I can't do that.");
+         }
+         else if (iArraySplit.includes("2")){
+           resultArray.push(" boop!");
+         }
+         else if (iArraySplit.includes("1")){
+           resultArray.push(" beep!");
+         }
+         else {
+           resultArray.push( i );
+         }
+         var result = resultArray.toString();
+       } return result;
+     }
 
-        for (var i =0; i <= userNumInt; i++){
+    // User Interface logic
 
-          iArray = [i].toString('');
-          var iArraySplit = iArray.split('');
-
-          if  (  iArray === "13"   ) {
-            resultArray.push(" I'm sorry, Dave. I'm afraid I can't do that.");
-
-          }
-
-          else if  (  iArray === "21"   ) {
-            resultArray.push(" I'm sorry, Dave. I'm afraid I can't do that.");
-
-          }
-
-          else if (iArraySplit.includes("3")){
-            resultArray.push(" I'm sorry, Dave. I'm afraid I can't do that.");
-
-          }
-
-          else if (iArraySplit.includes("2")){
-            resultArray.push(" boop!");
-          }
-
-          else if (iArraySplit.includes("1")){
-            resultArray.push(" beep!");
-
-          }
-          else {
-            resultArray.push( i );
-
-          }
-
-          var result = resultArray.toString();
-
-
-
-        }
-
-document.getElementById("output").innerHTML = result;
-
-  });
-
-});
+     // Get user input
+     var userNumInt = parseInt($("#userNumEntry").val());
+     // call business logic function for results
+     var result = makeResult(userNumInt);
+     // Output results
+     document.getElementById("output").innerHTML = result;
+   });
+ });
